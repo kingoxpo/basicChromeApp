7f33d3426a0f1ed40a510508e2c9870abf85a720
aiden <kingoxpo@gamil.com>
2021-12-27 15:27:00 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector("div.hey:first-child h1");
+
+function handleTitleClick() {
+  // const clickedClass = "active";
+  // if (h1.classList.contains(clickedClass)) {
+  //   h1.classList.remove(clickedClass);
+  // } else {
+  //   h1.classList.add(clickedClass);
+  // }
+  h1.classList.toggle("active");
+}
+
+h1.addEventListener("click", handleTitleClick);
