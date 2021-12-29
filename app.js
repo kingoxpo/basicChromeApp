1ba2d47b33cb99c23a96e00b820c91dd598b8742
aiden <kingoxpo@gamil.com>
2021-12-29 11:24:07 +0900

    finished user Login part

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERID_KEY = "userId";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const userId = loginInput.value;
+  localStorage.setItem(USERID_KEY, loginInput.value);
+  welcomeText(userId);
+}
+
+function welcomeText(userId) {
   greeting.innerText = `Welcome ${userId}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUserId = localStorage.getItem(USERID_KEY);
+
+if (savedUserId === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  welcomeText(savedUserId);
+}
