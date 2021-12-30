7e2dc581bdaec6407530bff99bb2676b58a50a9c
aiden <kingoxpo@gamil.com>
2021-12-30 15:33:32 +0900

    add clock - 12hours system

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,13 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  const second = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `현재 시간 ${date.getHours()}:${date.getMinutes()}:${second}`;
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  let hours = date.getHours();
+  let AMPM = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  clock.innerText = `현재시간 ${AMPM} ${hours}:${minutes}:${seconds}`;
   clock.classList.remove("hidden");
 }
 getClock();
---
+++ js/quotes.js
@@ -0,0 +1,44 @@
+const quotes = [
+  {
+    quote: "누구나 다 그럴듯한 계획이 있다. 처맞기 전까지는",
+    author: "Mike Tyson",
+  },
+  {
+    quote: "끝날 때까지 끝난 게 아니다.",
+    author: "Yogi Berra",
+  },
+  {
+    quote: "폼은 일시적이지만, 클래스는 영원하다.",
+    author: "Bill Shankly",
+  },
+  {
+    quote: "할 수 있다.",
+    author: "Simon Kuper",
+  },
+  {
+    quote: "내려갈 팀은 내려간다.",
+    author: "김재박",
+  },
+  {
+    quote: "내가 선을 긋는 순간, 나의 한계가 결정된다.",
+    author: "심권호",
+  },
+  {
+    quote: "강한팀이 이기는게 아니라 이긴팀이 강한 것이다.",
+    author: "Franz Anton Beckenbauer",
+  },
+  {
+    quote: "나보다 더 땀을 흘린 선수가 있다면, 금메달을 가져가도 좋다.",
+    author: "김현우",
+  },
+  {
+    quote:
+      "나는 인생에서 실패를 거듭했다. 그리고 그것이 바로 내가 성공한 이유다.",
+    author: "michael jordan",
+  },
+  {
+    quote:
+      "승리로 아주 조금 배울 수 있는 것들이 있다. 하지만 패배를 통해선 모든 걸 배울 수 있다.",
+    author: "Christy Mathewson",
+  },
+];
